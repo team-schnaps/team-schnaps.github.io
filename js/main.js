4b806fd8c593b41c5a0fe5ed1302a7eab40f1b7c
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2023-09-30 19:57:52 +0200

    make slogan rotation change by random chars

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
     new ScrollPropertyManager();
-    new TextRotator(document.querySelector('span.slogan'), 4000);
+    new TextRotator(document.querySelector('span.slogan'), 5000);
     new DaytimeManager(document.getElementById('daytime-switcher'));
 });
 
@@ -90,39 +90,79 @@ class TextRotator {
         while (true) {
             for (let i = 0; i < texts.length; i++) {
                 const text = texts[i];
-                await Typewriter.clear(element, 30);
-                await Typewriter.write(element, text, 100);
+                await TextRandomChanger.change(element, text, 700);
                 await new Promise(resolve => setTimeout(resolve, intervalMs));
             }
         }
     }
 }
 
-class Typewriter {
+class TextRandomChanger {
     /**
      * @param {HTMLElement} element
      * @param {string} text
-     * @param {int} typeDelayMs
+     * @param {int} durationMs
+     * @param {int} changeDelayMs
      * @return {Promise<void>}
      */
-    static async write(element, text, typeDelayMs) {
-        element.innerHTML = '';
+    static async change(element, text, durationMs, changeDelayMs = 100) {
+        const elementTextArray = element.innerText.split('');
+        const newTextArray = text.split('');
 
-        for (let i = 0; i < text.length; i++) {
-            element.innerHTML += text.charAt(i);
-            await new Promise(resolve => setTimeout(resolve, typeDelayMs));
+        if (element.innerText.trim().length === 0) {
+            elementTextArray.push(...newTextArray);
+            this.#shuffleArray(elementTextArray);
+        }
+
+        const diff = elementTextArray.length - newTextArray.length;
+        const diffCount = Math.abs(diff);
+        if (diff < 0) {
+            elementTextArray.push(...Array(diffCount));
+        } else {
+            newTextArray.push(...Array(diffCount));
+        }
+
+        const indexesToChange = [...Array(elementTextArray.length).keys()];
+        this.#shuffleArray(indexesToChange);
+
+        const iterationCount = Math.trunc(durationMs / changeDelayMs);
+        let changeRatio = elementTextArray.length / iterationCount;
+        let charsToChange = 0;
+
+        for (let i = 0; i < iterationCount; i++) {
+            charsToChange += changeRatio;
+
+            if (charsToChange >= 1) {
+                let charsToChangeCount = Math.trunc(charsToChange);
+                charsToChange -= charsToChangeCount;
+
+                for (let i = 0; i < charsToChangeCount; i++) {
+                    const index = indexesToChange.pop();
+                    elementTextArray[index] = newTextArray[index];
+                }
+
+                element.innerHTML = elementTextArray.join('');
+                await new Promise(resolve => setTimeout(resolve, changeDelayMs));
+            }
+        }
+
+        if (charsToChange > 0) {
+            let charsToChangeCount = Math.ceil(charsToChange);
+            for (let i = 0; i < charsToChangeCount; i++) {
+                const index = indexesToChange.pop();
+                elementTextArray[index] = newTextArray[index];
+            }
+            element.innerHTML = elementTextArray.join('');
         }
     }
 
     /**
-     * @param {HTMLElement} element
-     * @param {int} typeDelayMs
-     * @return {Promise<void>}
+     * @param array
      */
-    static async clear(element, typeDelayMs) {
-        while (element.innerText.length > 0) {
-            element.innerHTML = element.innerText.slice(0, element.innerText.length-1);
-            await new Promise(resolve => setTimeout(resolve, typeDelayMs));
+    static #shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
         }
     }
 }
